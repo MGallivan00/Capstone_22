630bb4e85b018c9079605a2a0d0300c95069f8dd
MGallivan00 <maria.gallivan@gmail.com>
2022-02-07 09:43:37 -0700

    button visual added

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -1,9 +1,17 @@
 import './App.css';
+import { Container, Button } from 'react-floating-action-button'
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
+          <Container>
+              <Button
+                  tooltip="New Node"
+                  styles={{backgroundColor: "#00B1E1", color: "#FFFFFF"}}
+                  onClick={() => alert("Button Click")}
+              />
+          </Container>
       </header>
     </div>
   );
