7a984674ae135e84893d367cd343b0a07d614b4e
Zoe Norden <54187068+znorden17@users.noreply.github.com>
2022-02-13 19:15:54 -0700

    Update App.js

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -1,20 +1,41 @@
 import './App.css';
 import { Container, Button } from 'react-floating-action-button'
+import {
+   Menu,
+    MenuItem,
+    MenuButton,
+    SubMenu
+} from '@szhsin/react-menu';
+import '@szhsin/react-menu/dist/index.css';
+
+
+import React from "react";
 
 function App() {
+  
   return (
     <div className="App">
-      <header className="App-header">
-          <Container>
-              <Button
-                  tooltip="New Node"
-                  styles={{backgroundColor: "#00B1E1", color: "#FFFFFF"}}
-                  onClick={() => alert("Button Click")}
-              />
-          </Container>
-      </header>
+      <div className="Menu">
+      <Menu menuButton={
+      <MenuButton className="btn-primary">Menu</MenuButton>}>
+        <MenuItem>Load</MenuItem>
+        <SubMenu label="Preset">
+          <MenuItem>preset1.html</MenuItem>
+          <MenuItem>preset2.js</MenuItem>
+        </SubMenu>
+        <MenuItem>Save</MenuItem>
+        <MenuItem>Export </MenuItem>
+      </Menu>
     </div>
-  );
-}
+      <header className="App-header">
+        <Container>
+          <Button
+            tooltip="New Node"
+            styles={{ backgroundColor: "#00B1E1", color: "#FFFFFF" }}
+            onClick={() => alert("Button Click")} />
+        </Container>
+      </header>
 
-export default App;
+    </div>
+  )
+}export default App;
