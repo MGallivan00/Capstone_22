eea27137ddb7df102b9e99cef0f9a1f8a07fc758
MGallivan00 <maria.gallivan@gmail.com>
2022-02-14 08:35:23 -0700

    Button Functionality

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -1,17 +1,31 @@
+//citation for button click: https://upmostly.com/tutorials/calling-a-react-component-on-button-click
+
 import './App.css';
+import { ListNode } from "./ListNode";
+import { useState } from 'react';
 import { Container, Button } from 'react-floating-action-button'
 
 function App() {
+
+    const [nodes, setNode] = useState(["Node"]);
+    const [nodeNames, setNodeNames] = useState([ "Node" ]); //this will become the question
+
+
+    function addComponent(){
+            setNode([...nodes, nodeNames[0]]); //this will become the metadata
+    }
   return (
     <div className="App">
       <header className="App-header">
           <Container>
               <Button
                   tooltip="New Node"
-                  styles={{backgroundColor: "#00B1E1", color: "#FFFFFF"}}
-                  onClick={() => alert("Button Click")}
+                  icon="fas fa-plus"
+                  styles={{backgroundColor: "#00B1E1", color: "#FFFFFF"}} onClick={addComponent}
               />
           </Container>
+
+          {nodes.map((item, i) => ( <ListNode text={item} /> ))}
       </header>
     </div>
   );
---
+++ react-app/src/ListNode.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+const ListNode = (props) => {
+
+    return (
+
+        <div className="Node">
+
+            <h1>{props.text}</h1>
+
+        </div>
+
+    );
+
+};
+
+export {ListNode};
