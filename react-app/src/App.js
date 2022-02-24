451a2f679cac4ba1a26c87a89339f75caaf06d2a
Dawson0206 <67607490+Dawson0206@users.noreply.github.com>
2022-02-24 15:12:51 -0700

    read and write

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -1,5 +1,5 @@
 //citation for button click: https://upmostly.com/tutorials/calling-a-react-component-on-button-click
-
+import * as React from "react";
 import './App.css';
 import { ListNode } from "./ListNode";
 import { useState } from 'react';
@@ -7,27 +7,73 @@ import { Container, Button } from 'react-floating-action-button'
 import { Menu, MenuItem, MenuButton, SubMenu } from '@szhsin/react-menu';
 import '@szhsin/react-menu/dist/index.css';
 
-function App() {
+
+
+import{getDatabase,ref, set, get, snapshot, onValue,getDocs} from "firebase/database";
+
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyALd8fTT_YORi0wwJ7bC_7O347ssGlItvg",
+  authDomain: "capstone-pique.firebaseapp.com",
+  projectId: "capstone-pique",
+  storageBucket: "capstone-pique.appspot.com",
+  messagingSenderId: "981342029277",
+  appId: "1:981342029277:web:e5b3373ffd6425e60c2234",
+  measurementId: "G-S0HHKBR67N"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+const database=getDatabase();
+
+
+
+
+function App(){
 
     const [nodes, setNode] = useState(["Node"]);
     const [nodeNames, setNodeNames] = useState([ "Node" ]); //this will become the question
 
-
     function addComponent(){
             setNode([...nodes, nodeNames[0]]); //this will become the metadata
     }
+
+    function load_file(){
+        var name=window.prompt("Enter file name ");
+        const test = ref(database, name +'/');
+        return onValue(test), (snapshot) =>{
+            const test2 = (snapshot.val() && snapshot.val().test2) || 'Testing';
+        },
+        {onlyOnce:true}
+    };
+
+    function write_file(){
+        var name=window.prompt("Enter the file name: ");
+        set(ref(database, name +'/'),{
+            name:name,
+        });
+    }
+
+
   return (
       <div className="App">
           <div className="Menu">
-              <Menu menuButton={
-                  <MenuButton className="btn-primary">Menu</MenuButton>}>
+              <Menu menuButton={<MenuButton className="btn-primary">Menu</MenuButton>}>
                   <MenuItem>Load</MenuItem>
                   <SubMenu label="Preset">
-                      <MenuItem>preset1.html</MenuItem>
-                      <MenuItem>preset2.js</MenuItem>
+                    <MenuItem id="csharp" value="test" onClick={load_file}>Csharp Model</MenuItem>
+                    <MenuItem id="bin" value="test" onClick={load_file}> Bin Model</MenuItem>
                   </SubMenu>
-                  <MenuItem>Save</MenuItem>
-                  <MenuItem>Export </MenuItem>
+                  <MenuItem onClick={write_file}>Save</MenuItem>
+                  <MenuItem>Export</MenuItem>
               </Menu>
           </div>
       <header className="App-header">
@@ -43,6 +89,9 @@ function App() {
       </header>
     </div>
   );
+
+
+
 }
 
 export default App;
--- react-app/src/index.js
+++ react-app/src/index.js
@@ -3,6 +3,8 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { BrowserRouter as Router } from "react-router-dom";
+
 
 ReactDOM.render(
   <React.StrictMode>
@@ -10,6 +12,14 @@ ReactDOM.render(
   </React.StrictMode>,
   document.getElementById('root')
 );
+
+ReactDOM.render(
+  <Router>
+    <App />
+  </Router>,
+  document.getElementById("root")
+);
+
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
