1b02fc5132e1e0e9374ec47e773c876f02510efc
MGallivan00 <maria.gallivan@gmail.com>
2022-02-27 16:51:58 -0700

    can add nodes with unique names

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -8,7 +8,7 @@ import { Menu, MenuItem, MenuButton, SubMenu } from '@szhsin/react-menu';
 import '@szhsin/react-menu/dist/index.css';
 
 
-import{getDatabase,ref, set, get, snapshot, onValue,getDocs} from "firebase/database";
+/*import{getDatabase,ref, set, get, snapshot, onValue,getDocs} from "firebase/database";
 
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
@@ -31,58 +31,116 @@ const firebaseConfig = {
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 const analytics = getAnalytics(app);
-const database=getDatabase();
+const database=getDatabase();*/
 
 function App(){
 
-    const [nodes, setNode] = useState(["Node"]);
-    const [nodeNames, setNodeNames] = useState([ "Node3", "Node" ]); //this will become the question
-
-    function addComponent(){
-            setNode([...nodes, nodeNames[0]]); //this will become the metadata
+    // with reference from https://www.delftstack.com/howto/javascript/arraylist-in-javascript/
+    // and https://developer.mozilla.org/en-US/docs/Web/API/Window/sessionStorage
+    var [nodes, setNode] = useState(["Root"]);
+    if(window.sessionStorage.getItem('storeNode') != null) {
+        nodes = window.sessionStorage.getItem('storeNode').split(",");
+        console.log(nodes);
+        //nodes.forEach(setPosition);
+    } else{
+        window.sessionStorage.setItem('storeNode', nodes.toString());
     }
 
-     function load_file(){
-         var name=window.prompt("Enter file name ");
-         const test = ref(database, name +'/');
-         return onValue(test), (snapshot) =>{
-             const test2 = (snapshot.val() && snapshot.val().test2) || 'Testing';
-         },
-         {onlyOnce:true}
-     };
+    function setPosition(item){
+        if(document.getElementById(item) != null) {
+            console.log(document.getElementById(item));
+            document.getElementById(item).style.position = 'absolute';
+            if(window.sessionStorage.getItem(item) != null) {
+                var positions = window.sessionStorage.getItem(item).split(",");
+                console.log(positions);
+                //console.log(positions[0]);
+                //console.log(positions[1]);
+                document.getElementById(item).style.left = positions[0];
+                console.log(document.getElementById(item));
+                document.getElementById(item).style.top = positions[1];
+            }
+        }
+        }
 
-     function write_file(){
-         var name=window.prompt("Enter the file name: ");
-         set(ref(database, name +'/'),{
-             name:name,
-         });
-     }
+        function addComponent(){
+            closeForm();
+            const elements = document.getElementById("form").elements;
+            nodes.unshift(elements[0].value);
+            console.log(nodes);
+            window.sessionStorage.setItem('storeNode', nodes.toString());
+            document.getElementById("form").reset();
+        }
 
-  return (
-      <div className="App">
-          <div className="Menu">
-              <Menu menuButton={<MenuButton className="btn-primary">Menu</MenuButton>}>
-                  <MenuItem>Load</MenuItem>
+        function openForm(){
+            document.getElementById("popup").style.display="block";
+        }
+
+        function closeForm(){
+            document.getElementById("popup").style.display="none";
+        }
+
+        /*     function load_file(){
+                 var name=window.prompt("Enter file name ");
+                 const test = ref(database, name +'/');
+                 return onValue(test), (snapshot) =>{
+                     const test2 = (snapshot.val() && snapshot.val().test2) || 'Testing';
+                 },
+                 {onlyOnce:true}
+             };
+
+             function write_file(){
+                 var name=window.prompt("Enter the file name: ");
+                 set(ref(database, name +'/'),{
+                     name:name,
+                 });
+             }*/
+
+        return (
+            <div className="App">
+                <div className="Menu">
+                    <Menu menuButton={<MenuButton className="btn-primary">Menu</MenuButton>}>
+                        {/*                  <MenuItem>Load</MenuItem>
                   <SubMenu label="Preset">
                     <MenuItem id="csharp" value="test" onClick={load_file}>Csharp Model</MenuItem>
                     <MenuItem id="bin" value="test" onClick={load_file}> Bin Model</MenuItem>
                   </SubMenu>
                   <MenuItem onClick={write_file}>Save</MenuItem>
-                  <MenuItem>Export</MenuItem>
-              </Menu>
-          </div>
-      <div className="PlaySpace">
-          <Container>
-              <Button
-                  tooltip="New Node"
-                  icon={"plus"}
-                  styles={{backgroundColor: "#00B1E1", color: "#FFFFFF"}} onClick={addComponent}
-              />
-          </Container>
-          {nodes.map((item, i) => ( <ListNode key={i} text={item} /> ))}
-      </div>
-    </div>
-  );
-}
+                  <MenuItem>Export</MenuItem>*/}
+                    </Menu>
+                </div>
+                <div className="PlaySpace">
+                    <div className="form-popup" id="popup">
+                        <form className="form-container" method="get" id="form">
+                            <h2>InputNodeInfo</h2>
 
-export default App;
+                            <b>Name</b>
+                            <input type="text"
+                                   placeholder="NodeName"
+                                   name="name"/>
+
+                            <b>OtherInfo</b>
+                            <input type="text"
+                                   placeholder="OtherInfo"
+                                   name="info"/>
+
+                            <Button
+                                tooltip="Submit"
+                                styles={{backgroundColor:"#04AA6D",color:"#FFFFFF"}} onClick={addComponent}
+                            />
+                        </form>
+                    </div>
+
+                    <Container>
+                        <Button
+                            tooltip="New Node"
+                            icon={"plus"}
+                            styles={{backgroundColor: "#00B1E1", color: "#FFFFFF"}} onClick={openForm}
+                        />
+                    </Container>
+                    {nodes.map((item, i) => ( <ListNode key={i} text={item} /> ))}
+                </div>
+            </div>
+        );
+    }
+
+    export default App;
