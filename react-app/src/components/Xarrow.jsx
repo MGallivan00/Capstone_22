815388c978548265305cb105f47389d2d542d20e
Connor Lowe <con.lowe406@gmail.com>
2022-03-28 16:00:20 -0600

    Added linking functionality. Rewrote the node class to work with the linking. Added a file that defines the drop-down "TopBar" that presents the user with node options. Another file, XArrow, handles the behavior and color of the links themselves.

---
+++ react-app/src/components/Xarrow.jsx
@@ -0,0 +1,26 @@
+import React, { useState } from 'react';
+import Xarrow from 'react-xarrows';
+
+export default ({ setSelected, selected, line: { props } }) => {
+    const [state, setState] = useState({ color: 'coral' });
+    const defProps = {
+        passProps: {
+            className: 'xarrow',
+            onMouseEnter: () => setState({ color: 'IndianRed' }),
+            onMouseLeave: () => setState({ color: 'coral' }),
+            onClick: (e) => {
+                //so only the click event on the node will fire on not on the container itself
+                e.stopPropagation();
+                setSelected({
+                    id: { start: props.root, end: props.end },
+                    type: 'arrow',
+                });
+            },
+            cursor: 'pointer',
+        },
+    };
+    let color = state.color;
+    if (selected && selected.type === 'arrow' && selected.id.root === props.root && selected.id.end === props.end)
+        color = 'red';
+    return <Xarrow {...{ ...defProps, ...props, ...state, color, showHead: false, path: "straight"}} />;
+};
